c3b853d16e94277c9df4a45ef58ee30db5b37afa
Theophileaseh <theophilenwachan@gmail.com>
2022-05-06 23:27:40 +0100

    Update README and API

--- src/components/AllBooks.js
+++ src/components/AllBooks.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getBooks } from '../redux/books/books';
 import Book from './Book';
+import './AllBook.css';
 
 const AllBooks = () => {
   const { books, loading } = useSelector((state) => state.bookReducer);
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { FaCircleNotch } from 'react-icons/fa';
 import { removeBook } from '../redux/books/books';
+import './Book.css';
 
 const Book = (props) => {
   const {
@@ -16,24 +18,26 @@ const Book = (props) => {
 
   return (
     <div className="book" key={id}>
-      <div className="bookName">
+      <div className="bookNames">
         <p className="category">{category}</p>
         <h2 className="bookName">{title}</h2>
         <p className="author">{author}</p>
         <ul className="actions">
-          <li className="singleAction">
-            <button type="button" className="comments">Comments</button>
+          <li className="singleAction right">
+            <button type="button" className="comments btn">Comments</button>
+          </li>
+          <li className="singleAction right">
+            <button type="button" className="remove btn" onClick={deleteHandler}>Remove</button>
           </li>
           <li className="singleAction">
-            <button type="button" className="remove" onClick={deleteHandler}>Remove</button>
-          </li>
-          <li className="singleAction">
-            <button type="button" className="edit">Edit</button>
+            <button type="button" className="edit btn">Edit</button>
           </li>
         </ul>
       </div>
-      <div className="completed">
-        <div className="icon-complete" />
+      <div className="main-completed">
+        <div className="icon-complete">
+          <FaCircleNotch className="main-icon" />
+        </div>
         <div className="percent-complete">
           <p className="percent">{percent}</p>
           <p className="completed">Completed</p>

7f00b3c4d7eba410f2bbec9df5e08c8564739af8
Theophileaseh <theophilenwachan@gmail.com>
2022-06-09 07:48:52 +0100

    Fix linter error

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,9 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import bookReducer from './books/books';
 import categoryReducer from './categories/categories';
 
-
-const reducer = combineReducers(bookReducer, categoryReducer)
+const reducer = combineReducers(bookReducer, categoryReducer);
 const store = createStore(
   reducer,
   applyMiddleware(thunk),

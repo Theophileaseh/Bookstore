c666fc2637624c2f25fe0619d673c0f3d5a0a89c
Theophileaseh <theophilenwachan@gmail.com>
2022-06-09 09:28:17 +0100

    Add thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,14 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
 import categoryReducer from './categories/categories';
 
-const reducer = combineReducers(bookReducer, categoryReducer);
-const store = createStore(
-  reducer,
-  applyMiddleware(thunk),
-);
+const store = configureStore({
+  reducer: {
+    bookReducer,
+    categoryReducer,
+  },
+}, applyMiddleware(thunk));
 
 export default store;
